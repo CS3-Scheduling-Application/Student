ced9462dd548896395df9dae8b8a9bc2a6786aac
Krish54491 <krishbharal@gmail.com>
2023-12-06 12:03:26 -0600

    stuff

--- mysite/blog/connect.py
+++ mysite/blog/connect.py
@@ -1,33 +1,36 @@
 import sqlite3
 
-try:
 
-    sqliteConnection = sqlite3.connect('mysite/blog/database.db')
-    cursor = sqliteConnection.cursor()
-    print('Connected to DataBase')
+def sqlQuerey():
 
-    query = '''SELECT * FROM TEACHERS'''
-    cursor.execute(query)
-    
-    result = cursor.fetchall()
-    print('All the data from Math table:')
-    for row in result:
-        print(row)
+    try:
 
-    print('\nColumns in TEACHERS table:') 
-    data=cursor.execute('''SELECT Teacher_Name FROM TEACHERS''') 
-    for column in data.description: 
-        print(column) 
+        sqliteConnection = sqlite3.connect('mysite/blog/database.db')
+        cursor = sqliteConnection.cursor()
+        print('Connected to DataBase')
+
+        query = '''SELECT * FROM TEACHERS'''
+        cursor.execute(query)
         
+        result = cursor.fetchall()
+        print('All the data from Math table:')
+        for row in result:
+            print(row)
 
-    
-    cursor.close()
+        print('\nColumns in TEACHERS table:') 
+        data=cursor.execute('''SELECT Teacher_Name FROM TEACHERS''') 
+        for column in data.description: 
+            print(column) 
+            
 
-except sqlite3.Error as error:
-    print('Error occurred - ', error)
+        
+        cursor.close()
 
-finally:
+    except sqlite3.Error as error:
+        print('Error occurred - ', error)
 
-    if sqliteConnection:
-        sqliteConnection.close()
-        print('SQLite Connection closed')
+    finally:
+
+        if sqliteConnection:
+            sqliteConnection.close()
+            print('SQLite Connection closed')
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Blog
 from django.http import HttpResponse
+from .connect import sqlQuerey
+
 def index(request):
     return render(request, 'index.html')
 def bell(request):
@@ -11,4 +13,14 @@ def courserequests(request):
     return render(request,'courseRequests.html')
 def courseschedule(request):
     return render(request,'courseSchedule.html')
+
+def course_requests(request):
+    try:
+        result = sqlQuerey()  # Call the function from connect.py
+        # You can pass 'result' to your template context if you want to display it in the HTML
+
+        return render(request, 'courseRequests.html', {'result': result})
+
+    except Exception as e:
+        return HttpResponse(f"An error occurred: {str(e)}")
 
