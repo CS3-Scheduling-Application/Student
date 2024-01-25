3653912638d23a4d87b16ce1bc12dd5d49f341bc
Julian <jc.julian.carpenter@gmail.com>
2024-01-25 17:06:01 -0600

    added krish's changes

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -4,9 +4,10 @@ from . import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('homepage', views.index, name="homepage"),
-    path('bellSchedule', views.bell, name="bellSchedule"),
+    path('bellSchedule', views.bell, name="bell"),
     path('account', views.account, name="account"),
     path('courseSchedule', views.courseschedule, name="courseSchedule"),
     path('courseRequests', views.product_detail, name="product_detail"),
     path('courseRequests', views.get_classes, name="get_classes"),
+    path('submit_class_requests/', views.submit_class_requests, name='submit_class_requests'),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,118 +3,6 @@ from django.http import HttpResponse
 from .connect import sqlQuerey
 import sqlite3
 
-def product_detail(request):
-    sqliteConnection = sqlite3.connect('/Users/ramjashan/CSIII/Student Repository/Student/mysite/mysite/database.db')
-    try:
-        output = []
-
-        cursor = sqliteConnection.cursor()
-
-        """
-        query = '''SELECT * FROM TEACHERS'''
-        cursor.execute(query)
-        teacherResult = cursor.fetchall()
-        for row in teacherResult:
-            output.append(row)
-        """
-        query = '''SELECT * FROM ACC'''
-        cursor.execute(query)
-        result = cursor.fetchall()
-        for row in result:
-            new_row = (row[0], 'ACC', row[1])
-            output.append(new_row)
-        
-        
-        query = '''SELECT * FROM ATH'''
-        cursor.execute(query)
-        result = cursor.fetchall()
-        for row in result:
-            new_row = (row[0], 'Athletics', row[1])
-            output.append(new_row)
-        
-        
-        query = '''SELECT * FROM CTE'''
-        cursor.execute(query)
-        result = cursor.fetchall()
-        for row in result:
-            new_row = (row[0], 'Electives', row[1])
-            output.append(new_row)
-        
-        
-        query = '''SELECT * FROM ENG'''
-        cursor.execute(query)
-        result = cursor.fetchall()
-        for row in result:
-            new_row = (row[0], 'English', row[1])
-            output.append(new_row)
-        
-        
-        query = '''SELECT * FROM ESL'''
-        cursor.execute(query)
-        result = cursor.fetchall()
-        for row in result:
-            new_row = (row[0], 'English Second Language', row[1])
-            output.append(new_row)
-        
-        
-        query = '''SELECT * FROM FINE_ART'''
-        cursor.execute(query)
-        result = cursor.fetchall()
-        for row in result:
-            new_row = (row[0], 'Fine Arts', row[1])
-            output.append(new_row)
-        
-        
-        query = '''SELECT * FROM MATH'''
-        cursor.execute(query)
-        result = cursor.fetchall()
-        for row in result:
-            new_row = (row[0], 'Math', row[1])
-            output.append(new_row)
-        
-        
-        query = '''SELECT * FROM SCI'''
-        cursor.execute(query)
-        result = cursor.fetchall()
-        for row in result:
-            new_row = (row[0], 'Science', row[1])
-            output.append(new_row)
-        
-        
-        query = '''SELECT * FROM SOCIAL_STUDIES'''
-        cursor.execute(query)
-        result = cursor.fetchall()
-        for row in result:
-            new_row = (row[0], 'Social Studies', row[1])
-            output.append(new_row)
-        
-        
-        query = '''SELECT * FROM TECH'''
-        cursor.execute(query)
-        result = cursor.fetchall()
-        for row in result:
-            new_row = (row[0], 'Tech', row[1])
-            output.append(new_row)
-        
-        
-        query = '''SELECT * FROM WORLD_LANG'''
-        cursor.execute(query)
-        result = cursor.fetchall()
-        for row in result:
-            new_row = (row[0], 'World Languages', row[1])
-            output.append(new_row)
-        cursor.close()
-        
-        
-
-    except sqlite3.Error as error:
-        print('Error occurred - ', error)
-
-    finally:
-        if sqliteConnection:
-            sqliteConnection.close()
-            return render(request, 'adminfile/test.html', {'product': output})
-
 
 def index(request):
     return render(request, 'index.html')
@@ -127,7 +15,7 @@ def courseschedule(request):
     return render(request,'courseSchedule.html')
 
 def product_detail(request):
-    sqliteConnection = sqlite3.connect('/Users/ramjashan/CSIII/Student Repository/Student/mysite/mysite/database.db')
+    sqliteConnection = sqlite3.connect('C:\\Users\\bharal_909857\\Downloads\\Repository\\Student\\Student\\mysite\\blog\\database.db')
     try:
         output = []
 
@@ -262,4 +150,27 @@ def get_classes(request):
             sqliteConnection.close()
             print('SQLite Connection closed YAY')
             return render(request, 'courseSchedule.html', {'classes': output})
+
+def submit_class_requests(request):
+    if request.method == 'POST':
+        # Assuming 'class_select1' is the name attribute of your <select> element
+        classes= []
+        for i in range(11):
+            classes.append(request.POST.get('class_select'+str(i+1)))
+             
+
+        # Now 'class_one_value' contains the selected value from the form
+        print(f"Selected value for class_select1: {classes}")
+
+        # Assuming you need to pass some data to the template, create a context dictionary
+        context = {
+            'selected_class': classes,
+            # Add other data you want to pass to the template
+        }
+
+        # Render the template with the context
+        return render(request, 'courseRequests.html', context)
+
+    # Handle other HTTP methods or return a response for GET requests if needed
+    # ...
 
